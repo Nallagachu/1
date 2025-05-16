bbdb2cb8d7d5b88d6b5f597a766028d60e42d4c9
durgababu <durgababu.nallagachu@gmail.com>
2025-05-17 00:13:26 +0530

    A

---
+++ ty.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+
+
+
+echo "The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
+The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance."
