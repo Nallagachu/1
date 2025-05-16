9bc5cf9ebbab0d11533b0e9c31c6111207719ced
durgababu <durgababu.nallagachu@gmail.com>
2025-05-16 23:38:55 +0530

    A

---
+++ 3-var.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON=$3
+
+echo "The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance.
+The PERSON1 rises, the PERSON1 sets. The PERSON2 whispers, the PERSON3s dance."
