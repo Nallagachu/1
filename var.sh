7c0ca8e81dde452456d5f59ca7396cccb4fad895
durgababu <durgababu.nallagachu@gmail.com>
2025-05-17 00:00:18 +0530

    A

--- var.sh
+++ var.sh
@@ -2,11 +2,9 @@
 PERSON1=$1
 PERSON2=$2
 PERSON3=$3
- 
 
+echo "The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance."
 
-
-echo "The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
 The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
 The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
 The $PERSON1 rises, the $PERSON1 sets. The $PERSON2 whispers, the $PERSON3 dance.
