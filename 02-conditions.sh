341eb7298fed1201f53d9f736a0d2fecc32b1d58
durgababu <durgababu.nallagachu@gmail.com>
2025-05-17 12:19:03 +0530

    A

---
+++ 02-conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ NUMBER =$1
+    if [ $NUMBER -lt 10 ]
+    then
+        echo " Given $NUMBER is less then 10"
+        else
+        echo "Givem $NUMBER is greater then 10"
+    fi  
+    
