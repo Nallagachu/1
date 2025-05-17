4bbdbad2750c717c40070167326c6d3c239905d0
durgababu <durgababu.nallagachu@gmail.com>
2025-05-18 03:35:36 +0530

    A

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-USERID=$(id -u)  # Removed unnecessary space
+USERID=$(id -u)  # Removed space after '='
 
-if [ "$USERID" -ne 0 ]; then  # Fixed missing value in condition
+if [ "$USERID" -ne 0 ]; then  # Fixed condition brackets and added missing value
     echo "Error: Please run this script as root"
+    exit 1  # Exit to prevent unintended execution
 else
     echo "You are running this script as root"
 fi
 
-dnf install nginx -y
+dnf install nginx -y  # Ensures this runs only if the user is root
