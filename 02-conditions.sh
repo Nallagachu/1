19c731be8d00cba0d245d80968b6c3f2cee7396b
durgababu <durgababu.nallagachu@gmail.com>
2025-05-17 12:22:44 +0530

    A

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
- NUMBER =$1
+ NUMBER =$1 
+ 
+ -gt == greater than
+ -lt == less than
+ # -eq == equal to
+ # -ne == not equal to
+
     if [ $NUMBER -lt 10 ]
     then
         echo " Given $NUMBER is less then 10"
