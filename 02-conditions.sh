be1f28d6a58c3fe516cde1850ad7ba6e6808c2ec
durgababu <durgababu.nallagachu@gmail.com>
2025-05-17 12:25:05 +0530

    A

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -6,5 +6,5 @@ NUMBER=$1
 if [ $NUMBER -lt 10 ]; then
     echo "Given $NUMBER is less than 10"
 else
-    echo "Given $NUMBER is greater than or equal to 10"
+    echo "Given $NUMBER is less then 10"
 fi
