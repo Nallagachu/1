db2fee6182ed9514a46c586c189f6ef5c8de23c9
durgababu <durgababu.nallagachu@gmail.com>
2025-05-17 11:52:40 +0530

    A

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash 
 echo "All variables are strings in bash: $@"
 echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $(pwd)"
+echo "user running the script: $USER"
+echo "home directory of the user: $HOME"
+echo "process id of the script: $$"
+sleep 10 &
+echo "pid of the last background background: $!"
+ 
